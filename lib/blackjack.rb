fb73e51fc69da4b77c6323c2ef7cbcf17e98528e
Brian Rosengarten <barosengarten@gmail.com>
2018-11-26 03:54:19 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,6 +5,7 @@ end
 
 def deal_card
   # code #deal_card here
+  return rand(1..11)
 end
 
 def display_card_total
@@ -13,6 +14,7 @@ end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -42,4 +44,3 @@ end
 def runner
   # code runner here
 end
-    
